e70cb47def96a1382aeea1a51dc115d2843a861c
Nicole Freed <nicole.freed@gmail.com>
2017-07-04 22:05:44 -0700

    add skeleton app structure to main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:getconnected@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    body = db.Column(db.Longtext)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return "Hello"
+
+
+
+if __name__ == '__main__':
+    app.run()
